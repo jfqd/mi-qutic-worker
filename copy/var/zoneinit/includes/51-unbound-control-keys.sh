84b7903c84ecb563029dab1f7833b2db40ecf2cb
jfqd <jfqd@blun.org>
2018-01-06 20:47:13 +0100

    do unbound config while running zoneinit

--- copy/var/zoneinit/includes/51-unbound-control-keys.sh
+++ copy/var/zoneinit/includes/51-unbound-control-keys.sh
@@ -1,3 +1,13 @@
+# unbound config
+mkdir -p /opt/local/etc/unbound/local.d
+touch /opt/local/etc/unbound/root.hints
+touch /opt/local/etc/unbound/unbound.log
+chown unbound:unbound /opt/local/etc/unbound/
+chown unbound:unbound /opt/local/etc/unbound/root.hints
+chown unbound:unbound /opt/local/etc/unbound/unbound.log
+ln -nfs /opt/local/etc/unbound/unbound.log /var/log/unbound.log
+/opt/local/sbin/unbound-anchor
+
 # setup unbound-control keys
 /opt/local/sbin/unbound-control-setup
 
