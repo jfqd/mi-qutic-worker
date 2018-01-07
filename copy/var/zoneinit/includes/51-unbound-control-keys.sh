491a22bf0aca0c69e2233d9bd460ffddbe4a0d53
jfqd <jfqd@blun.org>
2018-01-07 11:22:18 +0100

    get root.hints from internic

--- copy/var/zoneinit/includes/51-unbound-control-keys.sh
+++ copy/var/zoneinit/includes/51-unbound-control-keys.sh
@@ -3,13 +3,17 @@ mkdir -p /opt/local/etc/unbound/local.d
 touch /opt/local/etc/unbound/root.hints
 touch /opt/local/etc/unbound/unbound.log
 chown unbound:unbound /opt/local/etc/unbound/
-chown unbound:unbound /opt/local/etc/unbound/root.hints
 chown unbound:unbound /opt/local/etc/unbound/unbound.log
 ln -nfs /opt/local/etc/unbound/unbound.log /var/log/unbound.log
-/opt/local/sbin/unbound-anchor
+
+wget ftp://ftp.internic.net/domain/named.cache -O /opt/local/etc/unbound/root.hints
+chown unbound:unbound /opt/local/etc/unbound/root.hints
+
+/opt/local/sbin/unbound-anchor 1>/dev/null 2>&1 || true
 
 # setup unbound-control keys
-/opt/local/sbin/unbound-control-setup
+echo "* executing unbound-control-setup"
+/opt/local/sbin/unbound-control-setup 1>/dev/null 2>&1 || true
 
 # enable unbound
 /usr/sbin/svcadm enable svc:/pkgsrc/unbound:default
