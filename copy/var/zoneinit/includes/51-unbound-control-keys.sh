00681e21632d543882cc841d6a6aeb9d995918ab
jfqd <jfqd@blun.org>
2021-12-23 14:34:25 +0100

    use conf.d

--- copy/var/zoneinit/includes/51-unbound-control-keys.sh
+++ copy/var/zoneinit/includes/51-unbound-control-keys.sh
@@ -1,10 +1,10 @@
 # unbound config
 mkdir -p /opt/local/etc/unbound/local.d
-touch /opt/local/etc/unbound/root.hints
-touch /opt/local/etc/unbound/unbound.log
-chown unbound:unbound /opt/local/etc/unbound/
-chown unbound:unbound /opt/local/etc/unbound/unbound.log
-ln -nfs /opt/local/etc/unbound/unbound.log /var/log/unbound_log
+mkdir -p /opt/local/etc/unbound/conf.d
+touch /opt/local/etc/unbound/conf.d/root.hints
+touch /opt/local/etc/unbound/conf.d/unbound.log
+chown -R unbound:unbound /opt/local/etc/unbound/conf.d
+ln -nfs /opt/local/etc/unbound/conf.d/unbound.log /var/log/unbound_log
 
 wget ftp://ftp.internic.net/domain/named.cache -O /opt/local/etc/unbound/root.hints || true
 chown unbound:unbound /opt/local/etc/unbound/root.hints || true
